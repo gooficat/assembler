97476a9a53369707113dabcbf8fd5a937336a9d8
Hasan Sabri <hsabri2007@gmail.com>
2026-02-13 14:50:11 +0000

    work on opcode table

---
+++ src/opc_tbl.s
@@ -0,0 +1,31 @@
+.data
+
+.globl mnem_tbl, mnem_tbl_end
+
+.include "src/prm.inc"
+.include "src/opc.inc"
+
+
+add: .asciz "add"
+
+mnem_tbl:
+    .quad add
+    .quad addclass
+mnem_tbl_end:
+
+
+
+addclass:
+    .word W|Mode16|Mode32|Mode64|Rex64
+    .byte 2
+    .long Imm8|Imm16|Imm32|Imm64
+    .long AL|Fixed|Reg8|Reg16|Reg32|Reg64
+    .byte 1
+    .byte 0x04
+
+    .word D|W|Mode16|Mode32|Mode64|Rex64
+    .byte 2
+    .long Reg8|Reg16|Reg32|Reg64|Byte|Word|DWord|QWord|RegMem|Disp
+    .long Reg8|Reg16|Reg32|Reg64
+    .byte 1
+    .byte 0x00
