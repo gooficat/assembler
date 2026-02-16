50221a85d20a33a261c1caecf77cdfbe3a8f8d76
Hasan Sabri <hsabri2007@gmail.com>
2026-02-16 00:56:30 +0000

    work on tokenizer

--- src/main.s
+++ src/main.s
@@ -5,17 +5,22 @@
 .include "token.inc"
 
 main:
-    sub $tok_strm_size + 40, %rsp
-
-
-    lea 40(%rsp), %rcx
+    sub $80, %rsp
+    lea 32(%rsp), %rcx
     lea test_path(%rip), %rdx
     call tok_strm_init
 
-    add $tok_strm_size + 40, %rsp
+    call tok_strm_next
 
+    mov %rcx, 72(%rsp)
+    lea tok_strm_token(%rcx), %rcx
+    call puts
+    mov 72(%rsp), %rcx
+
+    add $80, %rsp
     xor %rax, %rax
     ret
 
+.data
 
-test_path: .asciz "inc/token.inc"
+test_path: .asciz "C:/Users/User/Documents/assembler/inc/token.inc"
--- src/token.s
+++ src/token.s
@@ -10,25 +10,61 @@ tok_strm_init:
     push %rcx
     mov %rdx, %rcx
     lea fmode_rt(%rip), %rdx
-    sub $40, %rsp
+    sub $32, %rsp
     call fopen
-    add $40, %rsp
+    add $32, %rsp
     pop %rcx
     mov %rax, tok_strm_file(%rcx)
     call tok_strm_getc
+    call tok_strm_next
     ret
 
 tok_strm_getc:
     push %rcx
     mov tok_strm_file(%rcx), %rcx
+    sub $32, %rsp
     call fgetc
+    add $32, %rsp
     pop %rcx
     mov %eax, tok_strm_buffer(%rcx)
     ret
 
 tok_strm_next:
+    call tok_strm_strip_whitespace
+    lea tok_strm_token(%rcx), %rsi
+    cmpl $'0', tok_strm_buffer(%rcx)
+    jl tok_strm_not_alnum
+    cmpl $'z', tok_strm_buffer(%rcx)
+    jg tok_strm_not_alnum
+tok_strm_alnum_loop:
+    mov %al, (%rsi)
+    inc %rsi
+    call tok_strm_getc
+    cmpl $'0', tok_strm_buffer(%rcx)
+    jl tok_strm_done
+    cmpl $'z', tok_strm_buffer(%rcx)
+    jg tok_strm_done
+    jmp tok_strm_alnum_loop
+tok_strm_not_alnum:
+    je tok_strm_done
+    mov %al, (%rsi)
+    inc %rsi
+tok_strm_done:
+    movb $0, (%rsi)
     ret
 
+tok_strm_strip_whitespace:
+tok_strm_strip_whitespace_loop:
+    cmpl $' ', tok_strm_buffer(%rcx)
+    je tok_strm_strip_whitespace_strip
+    cmpl $'\n', tok_strm_buffer(%rcx)
+    je tok_strm_strip_whitespace_strip
+    cmpl $'\t', tok_strm_buffer(%rcx)
+    je tok_strm_strip_whitespace_strip
+    ret
+tok_strm_strip_whitespace_strip:
+    call tok_strm_getc
+    jmp tok_strm_strip_whitespace_loop
 
 tok_strm_close:
 
@@ -36,4 +72,4 @@ tok_strm_close:
 .data
 
 fmode_rt:
-    .asciz "rt"
+    .asciz "r"
