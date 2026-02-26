eeff351fe0ea6c3527e6dfe850a1b733cf9959ea
Hasan Sabri <hsabri2007@gmail.com>
2026-02-26 00:41:02 +0000

    more on opcode classes

--- src/opcodes.c
+++ src/opcodes.c
@@ -1,7 +1,9 @@
 #include "opcodes.h"
 
 Asm_Opcode opcode_table[] = {
-	{Mode16 | Mode32 | Mode64 | NeedsRex | NeedsModRM | FieldD | FieldW, {{}, {}}},
+	{{0x00}, 1, Mode16 | Mode32 | Mode64 | NeedsRex | NeedsModRM | FieldD | FieldW, {{Reg8|Reg16|Reg32|Reg64|RegMem|Disp8|Disp16|Disp32|Disp32S|BaseIndex}, {Reg8|Reg16|Reg32|Reg64}}},
 };
-Asm_Opcode_Class opcode_class_table[] = {};
+Asm_Opcode_Class opcode_class_table[] = {
+    {"add", 0, 1},
+};
 uint64_t		 num_opcode_classes	  = sizeof opcode_class_table / sizeof opcode_class_table[0];
